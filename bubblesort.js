2bf68fe8b1ab9dfdea889160eb97ffbf4933f751
lokeshjpt <lokeshjpt@gmail.com>
2020-10-18 01:12:41 +0000

    added few algos

---
+++ bubblesort.js
@@ -0,0 +1,20 @@
+
+// bubble sort
+let arr = [15, 12, 14, 29, 17, 10, 28];
+
+console.log('Input array : ', arr);
+
+for(i=arr.length-1; i > 0; i--){
+  for(j=0; j<i; j++){
+      if(arr[j] > arr[i])
+        swap(i,j);
+  }
+}
+
+function swap(i, j){
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+console.log('Elements after sorting: ', arr);
--- index.js
+++ index.js
@@ -8,11 +8,5 @@ function factorial(n){
 
 console.log('factorial(5) is ', factorial(5));
 
-let arr = new Array();
 
-arr.push(10);
-arr.push(15);
-arr.push(17);
-arr.push(18);
 
-console.log(arr);
